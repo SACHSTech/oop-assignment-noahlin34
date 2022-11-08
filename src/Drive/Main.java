7d07cc6fd553da555c036f14be45b7092533f843
noahlin34 <noahlin23@ycdsbk12.ca>
2022-11-08 09:03:26 -0500

    intialized main class

---
+++ src/Drive/Main.java
@@ -0,0 +1,21 @@
+package Drive;
+
+import java.io.*;
+import Drive.*;
+
+/**
+* 
+* This is the main class file
+* @author: Noah Lin
+*/
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("----- GOOGLE DRIVE CLI -----");
+
+    }
+}
