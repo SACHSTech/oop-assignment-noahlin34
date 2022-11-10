c616407103774bf2f21549af6ec98817176af751
noahlin34 <noahlin23@ycdsbk12.ca>
2022-11-10 18:38:59 -0500

    main file set up with basic print

--- src/Drive/Main.java
+++ src/Drive/Main.java
@@ -15,7 +15,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.println("----- GOOGLE DRIVE CLI -----");
 
+        System.out.println("Welcome to the Google Drive Command Line interface. Please initalize your drive by providing the following values.");
+
+        System.out.print("How many files does your drive contain?");
+        int intFileCount = Integer.parseInt(keyboard.readLine());
     }
 }
