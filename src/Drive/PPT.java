1ce61dc222e99c9cda290b23ef347c2380c90714
noahlin34 <noahlin23@ycdsbk12.ca>
2022-11-10 09:32:02 -0500

    added ppt class and constructor

---
+++ src/Drive/PPT.java
@@ -0,0 +1,20 @@
+package Drive;
+import Drive.*;
+
+public class PPT extends Document {
+    
+
+    private int intSlideCount;
+    private String strTheme;
+
+    public PPT(int wordCount, double fileSize, String owner, String fileExtension, int slideCount, String strTheme) {
+        super(wordCount, fileSize, owner, fileExtension);
+
+        this.intSlideCount = slideCount;
+        this.strTheme = strTheme;
+        
+    }
+
+
+
+}
