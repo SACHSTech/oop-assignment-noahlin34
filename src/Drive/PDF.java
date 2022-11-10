935fa3260ffe67c733b28379fc75bf87a16fb216
noahlin34 <noahlin23@ycdsbk12.ca>
2022-11-10 18:31:34 -0500

    added password and getter methods to pdf

--- src/Drive/PDF.java
+++ src/Drive/PDF.java
@@ -1,8 +1,15 @@
 package Drive;
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import Drive.*;
 
 
 public class PDF extends Document {
+
+    static BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in)); 
 
 
     private boolean boolIsProtected;
@@ -14,6 +21,29 @@ public class PDF extends Document {
         this.boolIsProtected = boolIsProtected;
         this.strPassword = strPassword;
         this.boolPassCheck = false;
+    }
+
+    public String getPassword() {
+        return this.strPassword;
+    }
+
+    public boolean boolIsProtected() {
+        return this.boolIsProtected;
+    }
+
+    public void checkPass(String[] args) throws IOException {
+        
+
+         while(boolPassCheck = false) {
+            System.out.println("Please type password");
+            String input = keyboard.readLine();
+            if(input.equals(strPassword)) {
+                boolPassCheck = true;
+            } else {
+                continue;
+            }
+          }
+
     }
 
     
