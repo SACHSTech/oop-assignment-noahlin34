db9cbf7e09afba8542af8a3cb0286dc3483ec3be
noahlin34 <noahlin23@ycdsbk12.ca>
2022-11-10 12:27:58 -0500

    added getter methods to xlsx

--- src/Drive/XLSX.java
+++ src/Drive/XLSX.java
@@ -5,13 +5,28 @@ public class XLSX extends Document {
 
     private int intRowCount;
     private int intColumnCount;
-    private int intTotalCellcount;
 
+    
     public XLSX(int wordCount, double fileSize, String owner, String fileExtension, int intRowCount, int intColumnCount) {
         super(wordCount, fileSize, owner, fileExtension);
 
         this.intColumnCount = intColumnCount;
         this.intRowCount = intRowCount;
+
     }
+
+    public int getRowCount() {
+        return this.intRowCount;
+    }
+
+    public int getColumnCount() {
+        return this.intColumnCount;
+    }
+
+    public int getTotalCellCount() {
+        return this.intRowCount * this.intColumnCount;
+    }
+
+
     
 }
