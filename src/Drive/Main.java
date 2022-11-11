218bd4ed6bfb591ed6982a20681d635dcbca9659
noahlin34 <noahlin23@ycdsbk12.ca>
2022-11-10 20:34:09 -0500

    added code to add a spreadsheet

--- src/Drive/Main.java
+++ src/Drive/Main.java
@@ -36,26 +36,48 @@ public class Main {
 
         for(int x = intFileCount; x > 0; x--) {
             System.out.print("Please enter the type of item you would like to add ");
-
-            if(keyboard.readLine().equals("doc")) {
-                System.out.println("DOCX File added. How many words does this file contain?");
+            String input = keyboard.readLine();
+            if(input.equals("doc")) {
+                System.out.print("DOCX File added. How many words does this file contain? ");
                 int intWordCount = Integer.parseInt(keyboard.readLine());
-                System.out.println("how many paragraphs does this item have?");
+                System.out.print("how many paragraphs does this item have? ");
                 int intParagraphCount = Integer.parseInt(keyboard.readLine());
-                System.out.println("What type of justification does this file use (left, right, center)");
+                System.out.print("What type of justification does this file use (left, right, center) ");
                 String strJustification = keyboard.readLine();
 
                 Docx docx = new Docx(intWordCount, intWordCount * 1.01, strAccountName, ".docx", intParagraphCount, strJustification);
                 itemsToAdd.add(docx);
+            } else {
+
+                if(input.equals("powerpoint")) {
+                    System.out.print("Powerpoint file added. How many words does this file contain? ");
+                    int intWordCount = Integer.parseInt(keyboard.readLine());
+                    System.out.print("How many slides does this powerpoint have? ");
+                    int intSlideCount = Integer.parseInt(keyboard.readLine());
+                    System.out.print("What theme does this powerpoint file use? ");
+                    String strTheme = keyboard.readLine();
+    
+                    PPT ppt = new PPT(intWordCount, intWordCount * 1.01, strAccountName, ".ppt", intSlideCount, strTheme);
+                    itemsToAdd.add(ppt);
+                } else {
+                    if(input.equals("spreadsheet")) {
+                        System.out.print("Spreadsheet file added. How many words does this file contain? ");
+                        int intWordCount = Integer.parseInt(keyboard.readLine());
+                        System.out.println("How many rows does this spreadsheet have? ");
+                        int intRowCount = Integer.parseInt(keyboard.readLine());
+                        System.out.println("How many columns does this spreadsheet file have? ");
+                        int intColumnCount = Integer.parseInt(keyboard.readLine());
+
+                        XLSX xlsx = new XLSX(intWordCount, intWordCount * 1.01, strAccountEmail, ".xlsx", intRowCount, intColumnCount);
+
+
+                    }
+                }       
             }
 
-            if(keyboard.readLine().equals("powerpoint")) {
-                System.out.println("Powerpoint file added. How many words does this file contain?");
-                int intWordCount = Integer.parseInt(keyboard.readLine());
-                System.out.println("How many slides does this powerpoint have?");
-                int intSlideCount = Integer.parseInt(keyboard.readLine());
-                
-            }
+            
+
+            
 
         }
 
