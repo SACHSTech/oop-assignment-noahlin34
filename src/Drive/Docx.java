4528b9f2415eeb694d62f32b26087800581384e5
noahlin34 <noahlin23@ycdsbk12.ca>
2022-11-16 16:38:01 -0500

    comments added to docx

--- src/Drive/Docx.java
+++ src/Drive/Docx.java
@@ -10,19 +10,38 @@ import Drive.*;
 
 public class Docx extends Document  {
 
+    //initizlizing variables
     private int intParagraphCount;
     private String strIndentationType;
     
+    /**
+     * Constructor to create a docx file 
+     * @param wordCount - number of words, inherited from document
+     * @param fileSize - size of the file, inherited from document
+     * @param owner - owner of this file, inherited from document
+     * @param fileExtension - file extension, inherited from document
+     * @param paragraphCount - number of pagraphs in this docx
+     * @param indentationType - indentation type used in this docx
+     * @param strTitle - title of the file, inherited from document
+     */
     public Docx(int wordCount, double fileSize, String owner, String fileExtension, int paragraphCount, String indentationType, String strTitle) {
         super(wordCount, fileSize, owner, fileExtension, strTitle);
         intParagraphCount = paragraphCount;
         strIndentationType = indentationType;
     }
 
+    /**
+     * getter method for number of paragraphs
+     * @return number of paragraphs
+     */
     public int getParagraphCount() {
         return intParagraphCount;
     }
 
+    /**
+     * getter method for indentation type
+     * @return indentation method used 
+     */
     public String getIndentationType() {
         return strIndentationType;
     }
